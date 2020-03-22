b8d7533fd53198925aedee9b236990a6a1a44933
bluelagoon2772 <62506909+bluelagoon2772@users.noreply.github.com>
2020-03-22 14:31:36 +0100

    4.2 Coin Flip Streaks

---
+++ 4.2 coinFlipStreaks.py
@@ -0,0 +1,24 @@
+import random
+
+numberofStreaks=0
+#Create list of heads and tails
+for experimentNumber in range(10000):
+    list=[]
+    for i in range(0,100):
+        list.append(random.randint(0,1))
+        if list[i]==0:
+            list[i]='h'
+        else:
+            list[i]='t'
+
+    #Check for 6 heads or tails in a row
+    x=0
+    while x<int(len(list)-5):
+        if list[(x)]==list[(x+1)]==list[x+2]==list[x+3]==list[x+4]==list[x+5]:
+            numberofStreaks=numberofStreaks+1
+            x=x+6
+        else:
+            x=x+1
+
+
+print('Chance of streak: %s%%' % (numberofStreaks / 10000))
